3256aa89c98d3b6594457b7c22dc1d4455e436a9
rizzotto <rizzottogui@gmail.com>
2023-07-07 14:38:01 -0300

    feat: boilerplate

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { styled } from 'styled-components/native';
+
+const Title = styled.Text`
+  font-size: 24px;
+  text-align: center;
+  color: #BF4F74;
+`;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Title>Open up App.tsx to start working on your app!</Title>
       <StatusBar style="auto" />
+      <Text>KLAKAKAKALALAL</Text>
     </View>
   );
 }
