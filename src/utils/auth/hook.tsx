5954d217f1dd5e73a0d862de6646ea0dbdf5078b
João Leão <joaofleao@gmail.com>
2023-07-16 21:20:18 -0300

    feat: auth hook conditional

--- src/utils/auth/hook.tsx
+++ src/utils/auth/hook.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 import { AuthContext, type AuthContextInterface } from './context'
 
-export function useAuth(): React.ContextType<typeof AuthContext> {
-  return React.useContext(AuthContext) as AuthContextInterface
+export function useAuth(): AuthContextInterface {
+  const userAuthContext = React.useContext(AuthContext)
+
+  if (userAuthContext === null)
+    throw new Error('useAuth has to be used within <AuthContext.Provider>')
+  return userAuthContext
 }
