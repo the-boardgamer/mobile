bfc3fdfa435826dc6da2568b29e5ce5cd1297c9f
rizzotto <rizzottogui@gmail.com>
2023-07-29 09:02:41 -0300

    feat: initial version

---
+++ src/components/segmented_control/index.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import SegmentedControl from '@react-native-segmented-control/segmented-control'
+import { useTheme } from 'styled-components'
+
+import { styledActiveFont, styledFont, styledSegmented } from './styles'
+
+export interface Props {
+  //   color?: 'primary' | 'secondary' | 'background'
+  //   icon?: JSX.Element
+  //   label?: string
+  segments: string[]
+  onChangeSegment?: (label: string, index: number) => void
+  //   variant?: 'filled' | 'shade' | 'outlined' | 'ghost'
+}
+
+const defaultValues: Props = {
+  segments: undefined,
+  onChangeSegment: undefined,
+}
+
+export default function SegmentedControlTest(props: Props): JSX.Element {
+  const { onChangeSegment, segments, ...rest } = { ...defaultValues, ...props }
+  const [index, setIndex] = useState(0)
+
+  const theme = useTheme()
+
+  return (
+    <SegmentedControl
+      values={segments}
+      selectedIndex={index}
+      backgroundColor={theme.palette.background.variant}
+      fontStyle={styledFont(theme)}
+      activeFontStyle={styledActiveFont(theme)}
+      tintColor={theme.palette.primary.default}
+      onChange={(event): void => {
+        const currentIndex = event.nativeEvent.selectedSegmentIndex
+        setIndex(currentIndex)
+        onChangeSegment(segments[currentIndex], currentIndex)
+      }}
+      style={styledSegmented(theme)}
+      {...rest}
+    />
+  )
+}
---
+++ src/components/segmented_control/styles.ts
@@ -0,0 +1,21 @@
+import { FontStyle } from '@react-native-segmented-control/segmented-control'
+import { ViewStyle } from 'react-native'
+import { DefaultTheme } from 'styled-components'
+
+export const styledFont = (theme: DefaultTheme): FontStyle => ({
+  color: theme.palette.primary.default,
+  fontWeight: 'bold',
+  fontSize: theme.size.size4,
+})
+
+export const styledActiveFont = (theme: DefaultTheme): FontStyle => ({
+  color: theme.palette.primary.inverse,
+  fontWeight: 'bold',
+  fontSize: theme.size.size4,
+})
+
+export const styledSegmented = (theme: DefaultTheme): ViewStyle => ({
+  height: 58,
+  borderRadius: theme.radius.radius3,
+  backgroundColor: theme.palette.background.variant,
+})
