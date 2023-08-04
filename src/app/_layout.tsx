5040a2eb175f5a9ce4a33ba0accc36c2aa7665d7
João Leão <joaofleao@gmail.com>
2023-08-04 11:17:27 -0300

    feat: loading fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,28 @@
+import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 
 import { AuthProvider, ThemeProvider } from '@contexts'
 
-const RootLayout = (): JSX.Element | null => (
-  <ThemeProvider>
-    <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </AuthProvider>
-  </ThemeProvider>
-)
+const RootLayout = (): JSX.Element | null => {
+  const [loadedFonts] = useFonts({
+    'Quicksand-Light': require('@assets/fonts/quicksand/Quicksand-Light.ttf'),
+    'Quicksand-Regular': require('@assets/fonts/quicksand/Quicksand-Regular.ttf'),
+    'Quicksand-Medium': require('@assets/fonts/quicksand/Quicksand-Medium.ttf'),
+    'Quicksand-SemiBold': require('@assets/fonts/quicksand/Quicksand-SemiBold.ttf'),
+    'Quicksand-Bold': require('@assets/fonts/quicksand/Quicksand-Bold.ttf'),
+  })
+
+  if (loadedFonts) {
+    return (
+      <ThemeProvider>
+        <AuthProvider>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" />
+          </Stack>
+        </AuthProvider>
+      </ThemeProvider>
+    )
+  }
+}
 
 export default RootLayout
