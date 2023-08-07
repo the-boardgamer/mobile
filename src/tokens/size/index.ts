4fbaf3bc39d4f85f17677d7cfc1e8001855b6f3a
João Leão <joaofleao@gmail.com>
2023-08-07 09:55:52 -0300

    feat: more sizes added

--- src/tokens/size/index.ts
+++ src/tokens/size/index.ts
@@ -16,6 +16,19 @@ const size: SizeType = {
   size13: '52px',
   size14: '56px',
   size15: '60px',
+  size16: '64px',
+  size17: '68px',
+  size18: '72px',
+  size19: '76px',
+  size20: '80px',
+  size21: '84px',
+  size22: '88px',
+  size23: '92px',
+  size24: '96px',
+  size25: '100px',
+  size26: '104px',
+  size27: '108px',
+  size28: '112px',
 }
 
 export default size
