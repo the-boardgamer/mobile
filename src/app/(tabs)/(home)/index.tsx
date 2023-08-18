146f65f2377794fd64b30883ac3dcc26370a5b1f
rizzotto <rizzottogui@gmail.com>
2023-08-18 11:06:54 -0300

    feat: list_item component

--- src/app/(tabs)/(home)/index.tsx
+++ src/app/(tabs)/(home)/index.tsx
@@ -1,8 +1,18 @@
 import { Text, View } from 'react-native'
 
+import { IconStar } from '@components/icons'
+import ListItem from '@components/list_item'
+
 const Home = (): JSX.Element => (
   <View>
     <Text>Home</Text>
+    <ListItem
+      title="Takenoko"
+      publisher="Galapagos Jogos"
+      icon={<IconStar />}
+      coverImage={require('../../temp/list_cover.png')}
+      foregroundImage={require('../../temp/list_foreground.png')}
+    />
   </View>
 )
 
