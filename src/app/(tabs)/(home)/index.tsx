1f7853df3237d8c8e737aed8fcc3638082618ced
João Leão <joaofleao@gmail.com>
2023-08-18 10:50:37 -0300

    feat: add background color to screens

--- src/app/(tabs)/(home)/index.tsx
+++ src/app/(tabs)/(home)/index.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from 'react-native'
 
-const Home = (): JSX.Element => (
-  <View>
-    <Text>Home</Text>
-  </View>
-)
+import { useTheme } from '@contexts'
+
+const Home = (): JSX.Element => {
+  const { theme } = useTheme()
+
+  return (
+    <View style={{ backgroundColor: theme.palette.background.default, flex: 1 }}>
+      <Text>Home</Text>
+    </View>
+  )
+}
 
 export default Home
