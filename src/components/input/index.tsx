5bde1a8ab8e51e2fc9dbd5a41d9851dd5d23949d
João Leão <joaofleao@gmail.com>
2023-09-01 17:12:51 -0300

    fix: remove input require cycle

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,20 +1,6 @@
-import { TextInputProps } from 'react-native'
-
 import CollapsibleInput from './collapsible'
 import StandardInput from './standard'
-
-export interface Props extends TextInputProps {
-  collapsible?: boolean
-  icon?: JSX.Element
-  onChange?: (value) => void
-  outlined?: boolean
-  placeholder?: string
-}
-
-export const defaultValues: Props = {
-  collapsible: false,
-  outlined: false,
-}
+import { defaultValues, Props } from './types'
 
 export default function Input(props: Props): JSX.Element {
   const { collapsible, ...rest } = { ...defaultValues, ...props }
--- src/components/input/standard.tsx
+++ src/components/input/standard.tsx
@@ -1,5 +1,5 @@
-import { defaultValues, Props } from '.'
 import * as Styled from './styles'
+import { defaultValues, Props } from './types'
 import { useTheme } from '@contexts'
 
 export default function StandardInput(props: Props): JSX.Element {
