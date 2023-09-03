093076e3eb2ce6a0fff8b62a50e23bb125538c20
rizzotto <rizzottogui@gmail.com>
2023-09-03 09:22:51 -0300

    feat: add animation

--- src/app/(tabs)/(home)/index.tsx
+++ src/app/(tabs)/(home)/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Animated, Easing } from 'react-native'
 
 import Button from '@components/button'
 import Header from '@components/header'
@@ -37,7 +38,7 @@ const data = [
     image: require('../../../assets/temp/ticket.png'),
     cover: require('../../../assets/temp/list_cover.png'),
     foreground: require('../../../assets/temp/list_foreground.png'),
-    title: 'Ticket to Ride -',
+    title: 'Ticket to Ride - bug',
     publisher: 'Galapagos Jogos',
   },
   {
@@ -53,6 +54,40 @@ const data = [
 const Home = (): JSX.Element => {
   const [view, setView] = React.useState('masonry')
 
+  const transformValue = React.useRef(new Animated.Value(0)).current
+  const opacityValue = React.useRef(new Animated.Value(0)).current
+
+  const firstRenderAnimation = React.useCallback(() => {
+    Animated.timing(transformValue, {
+      toValue: 1,
+      duration: 800,
+      easing: Easing.ease,
+      useNativeDriver: false,
+    }).start()
+
+    Animated.timing(opacityValue, {
+      toValue: 1,
+      duration: 400,
+      easing: Easing.ease,
+      useNativeDriver: false,
+    }).start()
+  }, [transformValue, opacityValue])
+
+  React.useEffect(() => {
+    firstRenderAnimation()
+  }, [firstRenderAnimation])
+
+  function handleOnPressView(): void {
+    opacityValue.resetAnimation()
+    Animated.timing(opacityValue, {
+      toValue: 1,
+      duration: 800,
+      easing: Easing.ease,
+      useNativeDriver: false,
+    }).start()
+    setView((prev) => (prev === 'masonry' ? 'list' : 'masonry'))
+  }
+
   return (
     <Global.Screen edges={['top', 'left', 'right']}>
       <Header.Root>
@@ -63,11 +98,25 @@ const Home = (): JSX.Element => {
         <Button
           color="background"
           icon={<IconSearch />}
-          onPress={(): void => setView((prev) => (prev === 'masonry' ? 'list' : 'masonry'))}
+          onPress={handleOnPressView}
         />
       </Header.Root>
 
-      {view === 'masonry' ? <MasonryView data={data} /> : <ListView data={data} />}
+      <Animated.View
+        style={{
+          opacity: opacityValue,
+          transform: [
+            {
+              translateY: transformValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [100, 0],
+              }),
+            },
+          ],
+        }}
+      >
+        {view === 'masonry' ? <MasonryView data={data} /> : <ListView data={data} />}
+      </Animated.View>
     </Global.Screen>
   )
 }
