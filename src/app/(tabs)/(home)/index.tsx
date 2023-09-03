f48229748ed44c5c22168008a89230d8f6cce98e
rizzotto <rizzottogui@gmail.com>
2023-09-03 08:42:32 -0300

    feat: shelf screen

--- src/app/(tabs)/(home)/index.tsx
+++ src/app/(tabs)/(home)/index.tsx
@@ -1,54 +1,75 @@
-/* eslint-disable react-native/no-inline-styles */
-import { ScrollView, View } from 'react-native'
+import React from 'react'
 
 import Button from '@components/button'
-import GridItem from '@components/grid_item'
 import Header from '@components/header'
-import { IconStar } from '@components/icon'
+import { IconSearch } from '@components/icon'
+import ListView from '@components/list_view'
+import MasonryView from '@components/masonry_view'
 import Global from '@styles'
 
-const covers = [
-  require('../../../assets/temp/santorini.png'),
-  require('../../../assets/temp/montmartre.png'),
-  require('../../../assets/temp/takenoko.png'),
-  require('../../../assets/temp/ticket.png'),
-  require('../../../assets/temp/carcassone.png'),
+const data = [
+  {
+    id: 0,
+    image: require('../../../assets/temp/santorini.png'),
+    cover: require('../../../assets/temp/list_cover.png'),
+    foreground: require('../../../assets/temp/list_foreground.png'),
+    title: 'Santorini',
+    publisher: 'Galapagos Jogos',
+  },
+  {
+    id: 1,
+    image: require('../../../assets/temp/montmartre.png'),
+    cover: require('../../../assets/temp/list_cover.png'),
+    foreground: require('../../../assets/temp/list_foreground.png'),
+    title: 'Montmartre',
+    publisher: 'Galapagos Jogos',
+  },
+  {
+    id: 2,
+    image: require('../../../assets/temp/takenoko.png'),
+    cover: require('../../../assets/temp/list_cover.png'),
+    foreground: require('../../../assets/temp/list_foreground.png'),
+    title: 'Takenoko',
+    publisher: 'Galapagos Jogos',
+  },
+  {
+    id: 3,
+    image: require('../../../assets/temp/ticket.png'),
+    cover: require('../../../assets/temp/list_cover.png'),
+    foreground: require('../../../assets/temp/list_foreground.png'),
+    title: 'Ticket to Ride -',
+    publisher: 'Galapagos Jogos',
+  },
+  {
+    id: 4,
+    image: require('../../../assets/temp/carcassone.png'),
+    cover: require('../../../assets/temp/list_cover.png'),
+    foreground: require('../../../assets/temp/list_foreground.png'),
+    title: 'Carcassone',
+    publisher: 'Galapagos Jogos',
+  },
 ]
 
-const Home = (): JSX.Element => (
-  <Global.Screen>
-    <Header.Root>
-      <Button
-        color="background"
-        icon={<IconStar />}
-      />
-      <Header.TextContent>
-        <Header.Title>Hey Guilherme!</Header.Title>
-        <Header.Description>Explore top rated games by category</Header.Description>
-      </Header.TextContent>
-    </Header.Root>
+const Home = (): JSX.Element => {
+  const [view, setView] = React.useState('masonry')
 
-    <ScrollView>
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <View style={{ flex: 1, padding: 10 }}>
-          {covers.map((cover, i) => (
-            <GridItem
-              key={`${cover}-${i}`}
-              image={cover}
-            />
-          ))}
-        </View>
-        <View style={{ flex: 1, padding: 10 }}>
-          {covers.reverse().map((cover, i) => (
-            <GridItem
-              key={`${cover}-${i}`}
-              image={cover}
-            />
-          ))}
-        </View>
-      </View>
-    </ScrollView>
-  </Global.Screen>
-)
+  return (
+    <Global.Screen edges={['top', 'left', 'right']}>
+      <Header.Root>
+        <Header.TextContent>
+          <Header.Title>Hey Guilherme!</Header.Title>
+          <Header.Description>Explore top rated games by category</Header.Description>
+        </Header.TextContent>
+        <Button
+          color="background"
+          icon={<IconSearch />}
+          onPress={(): void => setView((prev) => (prev === 'masonry' ? 'list' : 'masonry'))}
+        />
+      </Header.Root>
+
+      {view === 'masonry' ? <MasonryView data={data} /> : <ListView data={data} />}
+    </Global.Screen>
+  )
+}
 
 export default Home
