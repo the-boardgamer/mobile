9ace1001f4965c253a5991d359894faad35b963c
João Leão <joaofleao@gmail.com>
2023-09-04 22:27:46 -0300

    fix: radius of the gridItem fixed

--- src/components/grid_item/styles.ts
+++ src/components/grid_item/styles.ts
@@ -2,8 +2,7 @@ import { Animated } from 'react-native'
 import { styled } from 'styled-components/native'
 
 export const Image = styled(Animated.Image)((props) => ({
-  borderTopLeftRadius: props.theme.radius.radius4,
-  borderBottomLeftRadius: props.theme.radius.radius4,
+  borderRadius: props.theme.radius.radius4,
   width: '100%',
   flex: 1,
   height: 'auto',
