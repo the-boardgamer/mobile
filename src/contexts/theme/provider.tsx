cb997f76a6245e4766c9afa4b0c53a336163810e
rizzotto <rizzottogui@gmail.com>
2023-09-11 11:53:14 -0300

    fix: theme switch

--- src/contexts/theme/provider.tsx
+++ src/contexts/theme/provider.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import { ThemeProvider as StyledThemeProvider } from 'styled-components/native'
 
-import { dark, radius, size, typography } from '../../tokens'
+import { dark, light, radius, size, typography } from '../../tokens'
 import { ModeType } from '../../tokens/palettes/types'
 import { ThemeContext, type ThemeContextType } from './context'
 import { ThemeType } from './types'
 
 export const ThemeProvider = (props: { children: React.ReactNode }): JSX.Element => {
-  const [mode, setMode] = useState<ModeType>('dark')
+  const [mode, setMode] = useState<ModeType>('light')
 
   const theme: ThemeType = {
-    palette: dark,
+    // change this later to be easier to change modes
+    palette: mode === 'dark' ? dark : light,
 
     radius,
     size,
