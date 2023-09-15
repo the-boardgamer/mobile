03f64ef529ca8878cf97360b265349737dbe2e29
Guilherme Rizzotto <43299202+rizzotto@users.noreply.github.com>
2023-09-15 10:32:21 -0300

    Update src/components/header/styles.ts
    Co-authored-by: João Flores de Leão <joaofleao@gmail.com>

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -6,7 +6,6 @@ export const Container = styled.View((props) => ({
   justifyContent: 'space-between',
   gap: props.theme.size.size3,
   marginHorizontal: props.theme.size.size5,
-  // marginBottom: props.theme.size.size6,
 }))
 
 export const TextContent = styled.View((props) => ({
